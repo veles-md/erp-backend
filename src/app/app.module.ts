6d3f3be6541c5e722d430b4c48ec77b154245a13
ecalcutin <>
2020-04-21 15:27:27 +0300

    added env, config. mongo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,37 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
+import config from 'src/config';
 import { ERPModule } from 'src/erp';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ERPModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:
+        process.env.NODE_ENV === 'development'
+          ? 'development.env'
+          : 'production.env',
+      load: [config],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        console.log(configService.get('database'));
+        return {
+          uri: configService.get<string>('database.connection'),
+          useUnifiedTopology: true,
+          useNewUrlParser: true,
+          useCreateIndex: true,
+        };
+      },
+      inject: [ConfigService],
+    }),
+    ERPModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
