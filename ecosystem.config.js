e2a0769eb11816492a46599e596c18decec3af5b
ecalcutin <>
2020-04-21 14:43:00 +0300

    update pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,10 @@ module.exports = {
     {
       name: 'erp-backend',
       script: 'dist/main.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
       env: {
         NODE_ENV: 'development',
       },
