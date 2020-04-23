4db40aa9aa133e05156d26df65b22f116a500b79
ecalcutin <>
2020-04-23 21:28:26 +0300

    added basic transaction schema & interface

--- src/erp/interfaces/transaction.interface.ts
+++ src/erp/interfaces/transaction.interface.ts
@@ -4,5 +4,7 @@ export interface Transaction {
   product: string;
   stock: string;
   change: number;
+  updatedAt?: Date;
+  createdAt?: Date;
 }
 export interface TransactionModel extends Transaction, Document {}
--- src/erp/schemas/transaction.schema.ts
+++ src/erp/schemas/transaction.schema.ts
@@ -4,27 +4,30 @@ import { ProductRef } from './product.schema';
 import { StockRef } from './stock.schema';
 
 export const TransactionRef = 'TransactionRef';
-export const TransactionSchema = new Schema(
-  {
-    product: {
-      type: Schema.Types.ObjectId,
-      ref: ProductRef,
-      required: true,
-    },
-    stock: {
-      type: Schema.Types.ObjectId,
-      ref: StockRef,
-      required: true,
-    },
-    change: {
-      type: Number,
-      required: true,
-      validate: {
-        validator: (value: number) => value !== 0,
-      },
+export const TransactionSchema = new Schema({
+  product: {
+    type: Schema.Types.ObjectId,
+    ref: ProductRef,
+    required: true,
+  },
+  stock: {
+    type: Schema.Types.ObjectId,
+    ref: StockRef,
+    required: true,
+  },
+  change: {
+    type: Number,
+    required: true,
+    validate: {
+      validator: (value: number) => value !== 0,
     },
   },
-  {
-    timestamps: true,
+  createdAt: {
+    type: Date,
+    default: Date.now,
   },
-);
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
