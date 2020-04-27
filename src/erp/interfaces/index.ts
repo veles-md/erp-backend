32eb404d0d3ca1beba6a3ed51a4ef189a1799a90
ecalcutin <>
2020-04-27 14:16:51 +0300

    add enums

--- src/erp/interfaces/index.ts
+++ src/erp/interfaces/index.ts
@@ -2,9 +2,9 @@ export { Category, CategoryModel } from './category.interface';
 export { Product, ProductModel } from './product.interface';
 export { Stock, StockModel } from './stock.interface';
 export { Waybill, WaybillModel } from './waybill.interface';
-export { WaybillItem } from './waybill-item.interface';
 export {
   Transaction,
   TransactionModel,
   ResidueOpts,
 } from './transaction.interface';
+export { WaybillAction, WaybillType, PriceType } from './enums';
