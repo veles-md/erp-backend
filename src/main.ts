0dbbcadaea9f203611b838f12938182e68a007ac
ecalcutin <>
2020-04-28 13:53:37 +0300

    adjusted pipe

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import { AppModule } from 'src/app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   app.setGlobalPrefix('api');
   await app.listen(3000);
 }
